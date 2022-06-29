4360efc22323217d3427c1cefbb00c619de6ff0d
Thaddaeus Fam <thaddaeusfamous@gmail.com>
2022-06-28 20:45:00 -0500

    algorithm

--- index.js
+++ index.js
@@ -27,28 +27,83 @@ let round = prompt("Pick between Rock, Paper, or Scissors:");
 const playerSelection = round;
 const computerSelection = computerPlay();
 
+let rockCase = playerSelection.toLowerCase() === rock;
+
 function playRound(playerSelection, computerSelection) {
-	if (playerSelection == rock && computerSelection == scissors) {
-		alert("You win! Rock beats scissors");
-		console.log(
-			`You picked ${playerSelection}, and the computer picked ${computerSelection}`
-		);
-	} else if (playerSelection == rock && computerSelection == paper) {
-		alert("You lose! Paper beats rock");
-		console.log(
-			`You picked ${playerSelection}, and the computer picked ${computerSelection}`
-		);
-	} else if (playerSelection == rock && computerSelection == rock) {
-		alert("It's a tie! You both picked rock");
-		console.log(
-			`You picked ${playerSelection}, and the computer picked ${computerSelection}`
-		);
+	switch (playerSelection) {
+		case "rock":
+			if (playerSelection == rock && computerSelection == scissors) {
+				alert("You win! Rock beats scissors");
+				console.log(
+					`You win! You picked ${playerSelection}, and the computer picked ${computerSelection}`
+				);
+			} else if (playerSelection == rock && computerSelection == paper) {
+				alert("You lose! Paper beats rock");
+				console.log(
+					`You lose! You picked ${playerSelection}, and the computer picked ${computerSelection}`
+				);
+			} else {
+				alert("It's a tie! You both picked rock");
+
+				console.log(
+					`It's a tie! You picked ${playerSelection}, and the computer picked ${computerSelection}`
+				);
+			}
+			break;
+		case "paper":
+			if (playerSelection == paper && computerSelection == paper) {
+				alert("You win! Paper beats rock");
+				console.log(
+					`You win! You picked ${playerSelection}, and the computer picked ${computerSelection}`
+				);
+			} else if (
+				playerSelection == paper &&
+				computerSelection == scissors
+			) {
+				alert("You lose! Scissors beats paper");
+				console.log(
+					`You lose! You picked ${playerSelection}, and the computer picked ${computerSelection}`
+				);
+			} else {
+				alert("It's a tie! You both picked paper");
+
+				console.log(
+					`It's a tie! You picked ${playerSelection}, and the computer picked ${computerSelection}`
+				);
+			}
+			break;
+		case "scissors":
+			if (playerSelection == scissors && computerSelection == paper) {
+				alert("You win! Scissors beats paper");
+				console.log(
+					`You win! You picked ${playerSelection}, and the computer picked ${computerSelection}`
+				);
+			} else if (
+				playerSelection == scissors &&
+				computerSelection == rock
+			) {
+				alert("You lose! Rock beats scissors");
+				console.log(
+					`You lose! You picked ${playerSelection}, and the computer picked ${computerSelection}`
+				);
+			} else {
+				alert("It's a tie! You both picked scissors");
+
+				console.log(
+					`It's a tie! You picked ${playerSelection}, and the computer picked ${computerSelection}`
+				);
+			}
+			break;
+
+		default:
+			alert("error");
+			console.log("error");
 	}
 }
-console.log(playRound(playerSelection, computerSelection));
+playRound(playerSelection, computerSelection);
 
 function game() {
-	playRound();
+	return playRound();
 }
 
 // Write a function that plays a single round of RPS
